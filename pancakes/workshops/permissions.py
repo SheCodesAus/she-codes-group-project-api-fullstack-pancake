5a595b3f2db1d95ce31a4db6982b10f1ca578963
Caroline99 <carolinejbirch@gmail.com>
2022-08-31 22:29:06 +1000

    fixing serializer and commenting out to basics

--- pancakes/workshops/permissions.py
+++ pancakes/workshops/permissions.py
@@ -1,8 +1,8 @@
-from rest_framework import permissions
+# from rest_framework import permissions
 
-class IsOrganiserOrReadOnly(permissions.BasePermission):
+# class IsOrganiserOrReadOnly(permissions.BasePermission):
     
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.organiser == request.user
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True
+#         return obj.organiser == request.user
--- pancakes/workshops/views.py
+++ pancakes/workshops/views.py
@@ -7,7 +7,7 @@ from .models import Workshop
 from .serializers import WorkshopSerializer, WorkshopDetailSerializer
 from django.http import Http404
 from rest_framework import status, permissions
-from .permissions import IsOrganiserOrReadOnly
+# from .permissions import IsOrganiserOrReadOnly
 
 # /workshops
 class WorkshopList(APIView):
@@ -23,37 +23,40 @@ class WorkshopList(APIView):
         #     return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = WorkshopSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(organiser=request.user)
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            serializer.save()
+            return Response(serializer.data)
+        #     serializer.save(organiser=request.user)
+        #     return Response(
+        #         serializer.data,
+        #         status=status.HTTP_201_CREATED
+        #     )
+        # return Response(
+        #     serializer.errors,
+        #     status=status.HTTP_400_BAD_REQUEST
+        # )
+            
 
 
 class WorkshopDetail(APIView):
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsOrganiserOrReadOnly
-    ]
+    # permission_classes = [
+    #     permissions.IsAuthenticatedOrReadOnly,
+    #     IsOrganiserOrReadOnly
+    # ]
     
-    # def get_object(self, pk):
-    #     try:
-    #         workshop = Workshop.objects.get(pk=pk) 
-    #         self.check_object_permissions(self.request,workshop)
-    #         return workshop  
-    #     except Workshop.DoesNotExist:
-    #         raise Http404
-
     def get_object(self, pk):
-        return Workshop.objects.get(pk=pk)
+        try:
+            workshop = Workshop.objects.get(pk=pk) 
+            # self.check_object_permissions(self.request,workshop)
+            return workshop  
+        except Workshop.DoesNotExist:
+            raise Http404
+
+    # def get_object(self, pk):
+    #     return Workshop.objects.get(pk=pk)
 
     # GET request    
     def get(self, request, pk):
         workshop = self.get_object(pk)
         # serializer = WorkshopDetailSerializer(workshop)
-        serializer = WorkshopSerializer(workshop)
+        serializer = WorkshopDetailSerializer(workshop)
         return Response(serializer.data)
