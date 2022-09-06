1f4483f31ed703b62507e9592e05806e4f3cfb99
Caroline99 <carolinejbirch@gmail.com>
2022-09-06 23:17:13 +1000

    still fixing email

--- pancakes/users/models.py
+++ pancakes/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    email = models.EmailField()
+    # email = models.EmailField()
     profile_image = models.CharField(max_length=500)
 
     def __str__(self):
