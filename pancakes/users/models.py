c16070fbb44d48fe4440c6b11aacc3816152ef70
Caroline99 <carolinejbirch@gmail.com>
2022-09-06 23:08:03 +1000

    added email, profile_image, first_name and last_name and updated user delete

--- pancakes/users/models.py
+++ pancakes/users/models.py
@@ -1,8 +1,10 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField()
+    profile_image = models.CharField(max_length=500)
 
     def __str__(self):
         return self.username
